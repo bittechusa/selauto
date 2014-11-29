7a8091712d69c1945057d3df0428c15ce0f4ef2a
Shipon <shossain4430@gmail.com>
2014-11-28 18:55:25 -0800

    first commit

---
+++ BlueflyAutomationProject/src/com/bfly/ui/automation/test/ProductPurchase.java
@@ -0,0 +1,15 @@
+package com.bfly.ui.automation.test;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ProductPurchase 
+{
+
+	public static void main(String[] args) 
+	{
+			FirefoxDriver driver = new FirefoxDriver();
+			driver.get("http://www.bluefly.com");
+
+	}
+
+}
