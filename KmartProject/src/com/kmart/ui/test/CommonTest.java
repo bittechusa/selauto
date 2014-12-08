7e2da972c89688e3f9e7e6315ab1476621ae2e3c
Shipon <shossain4430@gmail.com>
2014-12-08 01:08:47 -0800

    Shipon

---
+++ KmartProject/src/com/kmart/ui/test/CommonTest.java
@@ -0,0 +1,34 @@
+package com.kmart.ui.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CommonTest 
+{
+	FirefoxDriver fd;
+	public CommonTest(FirefoxDriver cTest)
+	{
+		fd=cTest;
+	}
+	public void type(By tp, String st)
+	{
+		fd.findElement(tp).sendKeys(st);
+	}//end of method
+	public void click(By ck)
+	{
+		fd.findElement(ck).click();
+		
+	}//end of method
+	public void click(WebElement we)
+	{
+		we.click();
+	}//end of method
+	
+	public void syso()
+	{
+		System.out.println("Next");
+	}
+	
+
+}
